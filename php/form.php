b2c4b9db21f25dee6463e4195adf9eaa7f5fa17f
Felipe Rodríguez <Danitronick22@gmail.com>
2024-03-12 21:12:29 -0500

    cuarto commit

--- php/form.php
+++ php/form.php
@@ -4,6 +4,7 @@ include("connect.php");
 
 if (isset($_POST['play'])) {
 
+    echo "Formulario enviado";
     
     if (
         strlen($_POST['p1']) >= 1 &&
@@ -19,9 +20,20 @@ if (isset($_POST['play'])) {
         $p2 = trim($_POST['p2']);
         $p3  = trim($_POST['p3']);
         $fecha = date("d/m/y");
+        echo "p1: " . $p1 . "<br>";
+        echo "p2: " . $p2 . "<br>";
+        echo "p3: " . $p3 . "<br>";
         $consulta = " INSERT INTO datos(p1, p2, p3, fecha) 
                     VALUES ('$p1', '$p2', '$p3', '$fecha')";
         $res = mysqli_query($conex, $consulta);
+
+        // Verificar si la consulta se ejecutó correctamente
+        if ($res) {
+            echo "Datos insertados correctamente";
+        } else {
+            echo "Error al insertar datos: " . mysqli_error($conex);
+        }
+        
         
         if($consulta){
             ?>
